5f3ce34d93c8188709f210e951110ac57229b991
yosishayev <yosishayev@gmail.com>
2023-07-24 15:07:32 +0300

    add button style, add supplier to preference list.

--- app/src/main/java/com/yosishay/wedding_app/CustomListView.java
+++ app/src/main/java/com/yosishay/wedding_app/CustomListView.java
@@ -2,7 +2,11 @@ package com.yosishay.wedding_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 //this xml make to design the way the image and the text will appear in the listview
 public class CustomListView extends AppCompatActivity {
@@ -11,5 +15,6 @@ public class CustomListView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_list_view);
+
     }
-}
+    }
